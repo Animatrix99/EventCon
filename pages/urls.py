0a877849a26e5f361fb4bbdb17f8cae95e8739f6
Jo'rayev Abdulaziz <pasedonpars@gmail.com>
2023-10-31 14:32:53 +0500

    31.10.2023

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import HomePageView
+from .views import *
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='index'),
+    path('performer/', PerformerPageView.as_view(), name='performer'),
+    path('about/', AboutPageView.as_view(), name='about'),
+    path('contact/', ContactPageView.as_view(), name='contact'),
+    path('blog/', BlogPageView.as_view(), name='blog'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,19 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'index.html'
+
+
+class PerformerPageView(TemplateView):
+    template_name = 'performer.html'
+
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
+
+
+class ContactPageView(TemplateView):
+    template_name = 'contact.html'
+
+
+class BlogPageView(TemplateView):
+    template_name = 'blog.html'
